b2c2cd5503d2d4442f482b44a5919d49d7e072c3
wookkii <wookkii@home>
2021-06-30 09:55:08 +1000

    Add paginator

--- src/app/contactmanager/components/notes/notes.component.ts
+++ src/app/contactmanager/components/notes/notes.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { Note } from '../../models/note';
 
@@ -8,12 +9,14 @@ import { Note } from '../../models/note';
   changeDetection: ChangeDetectionStrategy.OnPush,
   styleUrls: ['./notes.component.scss']
 })
-export class NotesComponent implements OnInit {
+export class NotesComponent implements OnInit, AfterViewInit {
 
   @Input() notes!: Note[];
 
   displayedColumns: string[] = ['id',  'title', 'date' ];
   dataSource!: MatTableDataSource<Note>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   constructor() { 
   }
@@ -24,6 +27,10 @@ export class NotesComponent implements OnInit {
 
   ngOnChanges(): void {
     this.dataSource = new MatTableDataSource<Note>(this.notes);
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
   }
 
 }
